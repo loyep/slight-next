aebf820655e258ee3ebad6d8b20adf299755fd48
Zack <mwl@live.com>
2020-10-17 16:38:50 +0800

    更新document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,41 +4,21 @@ import Document, {
 	Head,
 	Main,
 	NextScript,
-    DocumentContext,
-    DocumentProps,
-	DocumentInitialProps
 } from 'next/document';
 
 export default class SlightDocument extends Document {
-
-    static async getInitialProps(ctx: DocumentContext) {
-		const initialProps: DocumentInitialProps = await Document.getInitialProps(
-			ctx
-        );
-        console.log(initialProps)
-		return { ...initialProps };
-    }
-    
-    static renderDocument<P>(
-        DocumentComponent: new () => Document<P>,
-        props: DocumentProps & P
-      ): React.ReactElement {
-        props.isDevelopment = true
-        return super.renderDocument(DocumentComponent, props)
-      }
     
     render() {
-        return (
-          <Html>
-            <Head>
-              <meta content="Description goes here." />
-              <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <body>
-              <Main></Main>
-              <NextScript></NextScript>
-            </body>
-          </Html>
-        );
-      }
+      return (
+        <Html>
+          <Head>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <body>
+            <Main></Main>
+            <NextScript></NextScript>
+          </body>
+        </Html>
+      );
+    }
 }
