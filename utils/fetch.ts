1823d5b40ee158f33e50d4e5ab96ee063b698675
Zack <mwl@live.com>
2020-10-17 23:37:52 +0800

    调整接口

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { message } from 'antd';
 
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://i.loyep.com/api',
   // baseURL: process.env.BASE_URL || 'http://localhost:3001',
   withCredentials: true,
   timeout: 10000
