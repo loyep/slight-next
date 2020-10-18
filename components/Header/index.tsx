795f6c468520e95c76e9cdff8216fb9e0ff2780d
Zack <mwl@live.com>
2020-10-18 11:06:36 +0800

    调整menus

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,12 +2,12 @@ import { NextPage } from 'next';
 // import Router from 'next/router';
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react'
-import { Layout, Button, Tooltip} from 'antd'
+import { Layout, Button, Tooltip, Menu } from 'antd'
 import './index.less';
 import classnames from 'classnames'
 
-
-const {Header } = Layout
+const { Header } = Layout
+const { Item: MenuItem } = Menu
 
 interface SltHeaderProps {
   isCollapsed?: boolean
@@ -30,17 +30,19 @@ const SltHeader: NextPage<SltHeaderProps> = (props: SltHeaderProps) => {
     <Header className={headerClasses}>
     <div className="slt-container">
       <div className="slt-header-logo">
-        <a href="/" rel="home">
-          <img
-            className="logo"
-            src={
-              !scroll && fixed
-                ? 'https://cosy.demo.nicetheme.xyz/wp-content/themes/Cosy3.3.0/images/logo-dark.png'
-                : 'https://cosy.demo.nicetheme.xyz/wp-content/themes/Cosy3.3.0/images/logo.png'
-            }
-            alt="slight"
-          />
-        </a>
+        <Link href="/">
+          <a rel="home">
+            <img
+              className="logo"
+              src={
+                !scroll && fixed
+                  ? 'https://cosy.demo.nicetheme.xyz/wp-content/themes/Cosy3.3.0/images/logo-dark.png'
+                  : 'https://cosy.demo.nicetheme.xyz/wp-content/themes/Cosy3.3.0/images/logo.png'
+              }
+              alt="slight"
+            />
+          </a>
+        </Link>
       </div>
       <div className="slt-header-nav slt-header-nav-right">
         <ul className="slt-header-menu">
@@ -71,23 +73,23 @@ const SltHeader: NextPage<SltHeaderProps> = (props: SltHeaderProps) => {
       </div>
 
       <div className="slt-header-nav slt-header-nav-left">
-        {/* <a-menu className="slt-header-menu" mode="horizontal">
-          <a-menu-item>
-            <nuxt-link to="/category/reading">文字</nuxt-link>
-          </a-menu-item>
-          <a-menu-item>
-            <nuxt-link to="/category/music">音乐</nuxt-link>
-          </a-menu-item>
-          <a-menu-item>
-            <nuxt-link to="/category/games">游戏</nuxt-link>
-          </a-menu-item>
-          <a-menu-item>
-            <nuxt-link to="/category/videos">视频</nuxt-link>
-          </a-menu-item>
-          <a-menu-item>
-            <nuxt-link to="/topic">专题推荐</nuxt-link>
-          </a-menu-item>
-        </a-menu> */}
+        <Menu className="slt-header-menu" mode="horizontal">
+          <MenuItem>
+            <Link href="/category/reading">文字</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/category/music">音乐</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/category/games">游戏</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/category/videos">视频</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/topic">专题推荐</Link>
+          </MenuItem>
+        </Menu>
       </div>
     </div>
   </Header>
