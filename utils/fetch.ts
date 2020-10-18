db502901715d4265cd303fd000e1f5cd86e11d33
Zack <mwl@live.com>
2020-10-18 21:55:10 +0800

    更新

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -1,13 +1,26 @@
 import axios from 'axios'
 import { message } from 'antd'
 
-const instance = axios.create({
+const service = axios.create({
   baseURL: '/api',
   // baseURL: process.env.BASE_URL || 'http://localhost:3001',
   withCredentials: true,
-  timeout: 10000,
+  timeout: 30000,
 })
 
+// request interceptor
+service.interceptors.request.use(
+  (config) => {
+    // do something before request is sent
+    console.log('request=', JSON.stringify(config))
+    return config
+  },
+  (error) => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
 export default async function fetch(options: any): Promise<any> {
   if (options.useToken) {
     options.headers = {
@@ -16,7 +29,7 @@ export default async function fetch(options: any): Promise<any> {
   }
 
   return new Promise((resolve, reject) => {
-    instance(options)
+    service(options)
       .then((response) => {
         const { data, status } = response
         const success = status === 200 || status === 304 ? true : false
