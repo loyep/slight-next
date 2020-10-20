d090c485703f36ece6365c6deea09dca469b174b
Zack <mwl@live.com>
2020-10-20 12:04:32 +0800

    调整

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const next_1 = __importDefault(require("next"));
+const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
+const proxy_1 = __importDefault(require("./proxy"));
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
+const app = next_1.default({ dev });
+const handler = routes_1.default.getRequestHandler(app);
+app.prepare().then(() => {
+    const server = express_1.default();
+    const { createProxyMiddleware } = require("http-proxy-middleware");
+    Object.keys(proxy_1.default).forEach(context => {
+        server.use(createProxyMiddleware(context, proxy_1.default[context]));
+    });
+    server.all("*", (req, res) => handler(req, res));
+    server.listen(port);
+    console.log(`> Server listening at http://localhost:${port} as ${dev ? "development" : process.env.NODE_ENV}`);
+});
+//# sourceMappingURL=index.js.map
--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,14 @@
 module.exports = {
   plugins: {
     'postcss-flexbugs-fixes': {},
-    'postcss-css-variables': {},
     'postcss-preset-env': {
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: 'no-2009',
       },
       stage: 3,
       features: {
         'custom-properties': false,
       },
     },
-    'postcss-px-to-viewport': {
-      unitToConvert: 'px',    // 需要转换的单位
-      viewportWidth: 750,     // 设计稿宽度
-      unitPrecision: 3,       // 转换小数点精度
-      propList: [             // 需转换的属性， *为全部转换
-        '*'
-      ],
-      viewportUnit: 'vw',     // 转换后的单位
-      fontViewportUnit: 'vw', // 同上
-      selectorBlackList: [],  // 转换黑名单，用于屏蔽一些 css rule中不需要转换的 class(id,tag等)
-      minPixelValue: 1,       // 最小不转换单位，比如1px
-      mediaQuery: false,      // 是否转换@media中的内容
-      replace: true,
-      exclude: /(\/|\\)(node_modules)(\/|\\)/,  // 排除目录
-    }
   },
-};
+}
