dd33f93331ce90437620a6ae21a87626a200c6d4
Zack <mwl@live.com>
2020-10-22 11:30:18 +0800

    更新

--- next.config.js
+++ next.config.js
@@ -89,26 +89,5 @@ module.exports = withOffline(withBundleAnalyzer(withLess(withCSS({
     staticFolder: '/static',
     isDev, // Pass through env variables
   },
-  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-  generateInDevMode: true,
-  workboxOpts: {
-    swDest: 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'https-calls',
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
+  dontAutoRegisterSw: true 
 }))));
