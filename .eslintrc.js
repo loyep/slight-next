7765f90ae04066ab83384b279e87161315dafc6d
Zack <mwl@live.com>
2020-10-30 00:06:59 +0800

    更新依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
-  'root': true,
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+  root: true,
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
-  'plugins': [
-    '@typescript-eslint'
-  ],
-  'env': {
-    'browser': true,
-    "es2021": true,
-    'node': true,
-    'es6': true,
+  plugins: ['@typescript-eslint', 'react-hooks', 'react', 'import', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    es6: true,
   },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'jsx': true,
-    }
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    jsx: true,
+    useJSXTextNode: true,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  'settings': {
-    'react': {
-      'version': 'detect' // 自动读取已安装的react版本
-    }
+  settings: {
+    react: {
+      version: 'detect', // 自动读取已安装的react版本
+    },
   },
-  'rules': {
+  rules: {
     // eslint 配置文档 https://eslint.org/docs/rules/
-    'no-unused-vars': 0, 
-    "react/react-in-jsx-scope": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "react/prop-types": 0,
+    'no-unused-vars': 0,
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'react/prop-types': 0,
     'no-console': 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-unused-vars": 0
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 0,
   },
-};
+}
