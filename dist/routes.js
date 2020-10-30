a1516a9bf4acac6f70de9f83879ccec3a44347be
Zack <mwl@live.com>
2020-10-30 08:53:07 +0800

    更新

--- dist/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const nextRoutes = require("next-routes");
-const routes = (module.exports = nextRoutes());
-routes.add("home", "/");
-exports.default = routes;
-//# sourceMappingURL=routes.js.map
