67b3edc6731c07f489a415222388375279f34d87
Zack <mwl@live.com>
2020-10-31 22:51:03 +0800

    更新

--- components/Widget/Author/index.tsx
+++ components/Widget/Author/index.tsx
@@ -1,36 +1,48 @@
 import { CaretUpOutlined } from '@ant-design/icons'
-import { Row, Col } from 'antd'
+import { Row, Col, Tag } from 'antd'
 import styles from './index.scss'
 
-export default function AuthorWidget(props) {
+interface User {
+  avatar?: string
+  title?: string
+  displayName?: string
+  cover?: string
+  slug?: string
+}
+
+export interface AuthorProps {
+  user: User
+}
+
+const defaultProps: AuthorProps = {
+  user: {
+    cover:
+      'https://pandapro.demo.nicetheme.xyz/wp-content/themes/PandaPRO-1.1.1/images/default-cover.jpg',
+    avatar:
+      '//gravatar.loli.net/avatar/db0a42e95e32035a8bb677ece1186533?s=80&amp;d=mm&amp;r=g',
+    displayName: 'Loren',
+    title: '站长',
+  },
+}
+
+export default function AuthorWidget(props: AuthorProps) {
+  const { user } = { ...defaultProps, ...props }
   return (
     <div className={styles.widget}>
       <div className={styles.cover}>
         <div className="slt-media">
           <div className="slt-media-content">
-            <img
-              src={
-                'https://pandapro.demo.nicetheme.xyz/wp-content/themes/PandaPRO-1.1.1/images/default-cover.jpg'
-              }
-            />
+            <img src={user.cover} />
           </div>
         </div>
         <div className={styles.avatar}>
-          <img
-            alt=""
-            src="//gravatar.loli.net/avatar/db0a42e95e32035a8bb677ece1186533?s=80&amp;d=mm&amp;r=g"
-            className="avatar avatar-80 photo 80"
-            height="80"
-            width="80"
-          />
+          <img src={user.avatar} className="avatar avatar-80 photo 80" />
         </div>
       </div>
       <div className={styles.meta}>
-        <div className="h6 mb-3">
-          suxing
-          <small className="d-block">
-            <span className="badge badge-outline-primary mt-2">站长</span>
-          </small>
+        <div className={styles.user}>
+          <h6>{user.displayName}</h6>
+          <Tag>{user.title}</Tag>
         </div>
         <div className={styles.desc}></div>
         <Row className={styles.row}>
@@ -43,53 +55,25 @@ export default function AuthorWidget(props) {
               <small className={styles.rowCount}>文章</small>
             </a>
           </Col>
-
           <Col flex={1}>
             <a
               href="https://pandapro.demo.nicetheme.xyz/author/suxing"
               target="_blank"
             >
               <span className={styles.rowTitle}>29</span>
-              <small className={styles.rowCount}>文章</small>
+              <small className={styles.rowCount}>评论</small>
             </a>
           </Col>
-
           <Col flex={1}>
             <a
               href="https://pandapro.demo.nicetheme.xyz/author/suxing"
               target="_blank"
             >
               <span className={styles.rowTitle}>29</span>
-              <small className={styles.rowCount}>文章</small>
+              <small className={styles.rowCount}>获赞</small>
             </a>
           </Col>
         </Row>
-        <div className="row no-gutters text-center">
-          <a
-            href="https://pandapro.demo.nicetheme.xyz/author/suxing"
-            target="_blank"
-            className="col"
-          >
-            <span className="font-theme font-weight-bold text-md">29</span>
-            <small className="d-block text-xs text-muted">文章</small>
-          </a>
-          <a
-            href="https://pandapro.demo.nicetheme.xyz/author/suxing"
-            target="_blank"
-            className="col"
-          >
-            <span className="font-theme font-weight-bold text-md">0</span>
-            <small className="d-block text-xs text-muted">评论</small>
-          </a>
-          <a
-            href="https://pandapro.demo.nicetheme.xyz/author/suxing"
-            target="_blank"
-            className="col"
-          >
-            <span className="font-theme font-weight-bold text-md">58</span>
-            <small className="d-block text-xs text-muted">获赞</small>
-          </a>
-        </div>
       </div>
     </div>
   )
