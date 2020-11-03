fefec5f576163d0959f4e352fea7cd689b373779
Zack <mwl@live.com>
2020-11-03 21:39:26 +0800

    no message

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
-import config from '@/config'
+import { useSelector } from 'react-redux'
+import { RootState, ConfigState } from '@/store/types'
+const { name } = useSelector<RootState, ConfigState>((state: RootState) => state.config)
 
 export function generateTitle(...titleSegments: string[]): string {
-  titleSegments.push(config.title)
+  titleSegments.push(name)
   return titleSegments.filter((t) => t).join(' · ')
 }
