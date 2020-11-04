38ad353568c7c01c0d0f39f74259269d54cd360d
Zack <mwl@live.com>
2020-11-04 23:31:25 +0800

    更新依赖

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-var-requires */
-// const fs = require('fs')
-// const path = require('path')
-// const lessToJS = require('less-vars-to-js')
 
 const withLess = require('@zeit/next-less')
 const withSass = require('@zeit/next-sass')
-
-// const themeVariables = lessToJS(
-//   fs.readFileSync(
-//     path.resolve(__dirname, './assets/styles/variable.less'),
-//     'utf8'
-//   )
-// )
 
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
@@ -34,7 +24,6 @@ module.exports = withSass({
     lessLoaderOptions: {
       lessOptions: {
         javascriptEnabled: true,
-        modifyVars: {}, // make your antd custom effective
       },
     },
   })
